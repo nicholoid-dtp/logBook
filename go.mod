d8e51bfb0eb88cde288f399e1b8703d0fc696495
Ibrahim Oladipupo Qamardeen <kensharval@gmail.com>
2019-11-27 21:17:30 +0100

    Progress 21:17/Nov 27, 2019

---
+++ go.mod
@@ -0,0 +1,8 @@
+module github.com/nicholoid-dtp/logBook
+
+go 1.13
+
+require (
+	github.com/qamarian-dtp/err v0.4.0
+	github.com/vjeantet/jodaTime v0.0.0-20170816150230-be924ce213fb
+)
---
+++ logBook.go
@@ -0,0 +1,37 @@
+package logBook
+
+import (
+	"github.com/vjeantet/jodaTime"
+	"github.com/qamarian-dtp/err"
+	"io"
+	"time"
+)
+
+func New (logBook io.Writer) (LogBook) {
+	return LogBook {logBook}
+}
+
+type LogBook struct {
+	logBook io.Writer
+}
+
+func (b LogBook) Record (log []byte) (error) {
+	logTime := jodaTime.Format ("hh:mm:ss a Z / MMM dd, yyyy: ", time.Now ())
+
+	_, errX := b.logBook.Write ([]byte (logTime))
+	if errX != nil {
+		return err.New ("Log not recorded or log recorded partially.", nil, nil, errX)
+	}
+
+	_, errY := b.logBook.Write (log)
+	if errY != nil {
+		return err.New ("Log recorded partially.", nil, nil, errY)
+	}
+
+	_, errZ := b.logBook.Write ([]byte ("\n"))
+	if errZ != nil {
+		return err.New ("Log recorded partially.", nil, nil, errZ)
+	}
+
+	return nil
+}
